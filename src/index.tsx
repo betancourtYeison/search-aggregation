7da4273159bf39616bc101270f79f92f7e1a7560
Yeison Betancourt Solis <yeison.betancourt@cetoco.com>
2021-08-02 22:28:22 -0500

    Rewrite index

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { configureStore } from './frameworks';
+import { Provider } from 'react-redux';
+
+import App from './components/AppPresenter';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+const element: HTMLElement | null = document.getElementById('root');
+const ConnectedApp = (
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
+
+ReactDOM.render(ConnectedApp, element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
